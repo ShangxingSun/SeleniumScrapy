db3c1052b2cfeab973c2dd71c4887a5acce3145d
ShangxingSun <36012471+ShangxingSun@users.noreply.github.com>
2018-12-15 17:06:21 -0800

    getBasicData

--- seleniumScp.py
+++ seleniumScp.py
@@ -1,12 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+import bs4
 
 driver = webdriver.Chrome(executable_path='./chromedriver')
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
+url = 'https://mobile.bet365.com/?apptype=&appversion=&cb=1544921750#/AS/B12/'
+driver.get(url)
+#time.sleep(10)
+#elem = driver.find_element_by_class_name("ff-Competitor_Name")
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'Splash')))
+pSource= driver.page_source
+
+soup = bs4.BeautifulSoup(pSource, "html.parser")
+for data in soup.findAll(id='Splash'):
+    for res in data.find_all('span'):
+        print(res.text)
 driver.close()
